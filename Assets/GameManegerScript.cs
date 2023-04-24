4962c80978f14579a0a82d855983404d75745404
haruki <haruki@MSI>
2023-04-24 13:51:00 +0900

    3d移動

--- Assets/GameManegerScript.cs
+++ Assets/GameManegerScript.cs
@@ -1,77 +1,123 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Build;
 using UnityEngine;
 
 public class GameManegerScript : MonoBehaviour
 {
+    public GameObject playerPrefab;
+    int[,] map;
+    GameObject[,] field;
 
-    int[] map;
-    void PrintArreay()
+
+
+    Vector2Int GetPlayerIndex()
     {
-        string debugText = "";
-        for(int i = 0; i < map.Length; i++)
+        for (int y = 0; y < map.GetLength(0); y++)
         {
-            debugText += map[i].ToString() + ", ";
-        }
-        Debug.Log(debugText);
-    }
-    int GetPlayerIndex()
-    {
-        for (int i = 0; i < map.Length; i++)
-        {
-            if (map[i] == 1)
+
+            for (int x = 0; x < map.GetLength(1); x++)
             {
-                return i;
+
+                
+                if (field[y, x] != null && field[y, x].tag == "Player") { return new Vector2Int(x, y); }
+
             }
-            return -1;
+
         }
+        return new Vector2Int(-1, -1);
     }
 
-    bool MoveNumber(int number,int moveFrom,int moveTo)
-    {
-        if(moveTo<0||moveTo >= map.Length)
+
+
+    bool MoveNumber(string tag,Vector2Int moveFrom,Vector2Int moveTo)
+   {
+        if (moveTo.y < 0 || moveTo.y >= map.GetLength(0)) { return false; }
+        if (moveTo.x < 0 || moveTo.x >= map.GetLength(1)) { return false; }
+
+        if (field[moveTo.y, moveTo.x] != null && field[moveTo.y, moveTo.x].tag == "Box")
         {
-            return false;
+            Vector2Int velocity = moveTo - moveFrom;
+            bool success = MoveNumber(tag, moveTo, moveTo + velocity);
+
+            if (!success) { return false; }
         }
-        map[moveTo] = number;
-        map[moveFrom] = 0;
+
+        field[moveFrom.y, moveFrom.x].transform.position = 
+            new Vector3(moveTo.x, field.GetLength(0) - moveTo.y, 0);
+        field[moveTo.y, moveTo.x] = field[moveFrom.y, moveFrom.x];
+        field[moveFrom.y, moveFrom.x] = null;
         return true;
 
+
     }
-   
 
-
+  
     // Start is called before the first frame update
     void Start()
     {
-        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
-        PrintArreay();
+
+
+        map = new int[,]
+        {{0,0,0,0,0},
+         {0,0,1,0,0},
+         {0,0,0,0,0},
+
+        };
+
+        field = new GameObject[
+            map.GetLength(0), 
+            map.GetLength(1)
+            ];
+
+       
+
+        for (int y = 0;y< map.GetLength(0); y++){
+
+            for(int x= 0; x < map.GetLength(1); x++)
+            {
+               
+                if (map[y,x] == 1)
+                {
+                    field[y,x] = Instantiate(
+                    playerPrefab,
+                    new Vector3(x, map.GetLength(0)-y, 0),
+                    Quaternion.identity
+                    );
+                }
+            }
+            
+        }
+      
+      
 
     }
 
    
-
+    
     // Update is called once per frame
     void Update()
     {
        
-
-
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
            
-            int playerIndex = GetPlayerIndex;
+            Vector2Int playerIndex = GetPlayerIndex();
 
-            if (playerIndex < map.Length - 1)
-            {
-               
-                map[playerIndex + 1] = 1;
-                map[playerIndex] = 0;
-            }
-            PrintArreay();
+            MoveNumber("Player", playerIndex, playerIndex + new Vector2Int (1,0));
+
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
 
+            Vector2Int playerIndex = GetPlayerIndex();
+
+            MoveNumber("Player", playerIndex, playerIndex - new Vector2Int(1, 0));
+            
+        }
     }
+    
 }
